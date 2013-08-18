548972065ead62ace3d9e886a9f75899eff79ab5
Andrew Casal <andrewjcasal@gmail.com>
2013-08-17 17:21:47 -0700

    added express

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 var http     = require('http');
 var mongoose = require('mongoose');
+var express  = require('express');
 
+var app = express();
 mongoose.connect('mongodb://localhost/reflectupon');
 
-var Cat = mongoose.model('Cat', { name: String });
-
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function(err) {
+var Thought = mongoose.model('Thought', { description: String });  
  
-  if (err)
-   console.log(err);
+app.get('/', function(req, res) {
+  
+  var thought = new Thought({ description: 'Zildjian' });
+  thought.save(function(err) {
+   
+    if (err)
+     console.log(err);
+     
+     res.send( thought.description );
 
-  http.createServer( function(req,res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end( kitty.name );
-  }).listen(2000, '127.0.0.1');
-  console.log('Server running at http://127.0.0.1:2000');
-
+  });
+  
 });
 
+app.listen(2000);  
 
+console.log('Server running at http://127.0.0.1:2000');
+ 
