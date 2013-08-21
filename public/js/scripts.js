ea75f46b55f562968ea2f4c0c769523ee0e22c27
Andrew Casal <andrewjcasal@gmail.com>
2013-08-20 22:42:24 -0700

    Finally got backbone UI to work

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,60 @@
-$(document).ready(function(){
-  $.post("/api/thought", { name: "test", description: "woah" }, function(data) {
-    console.log(data);
-  },"json");
+$(document).ready(function() {
+  
+  var Thought = Backbone.Model.extend({
+    defaults: {
+      "description": "test"
+    },
+    urlRoot: '/api/thought/'
+  });
+
+  var Thoughts = Backbone.Collection.extend({
+    model: Thought,
+    url: '/api/thought'
+  });
+ 
+  var ThoughtView = Backbone.View.extend({
+
+    el: ".thoughts-list",
+    
+    events: {
+      'click .create-thought': 'createThought'
+    },
+
+    template: _.template("<li><%= description %></li>"),
+
+    initialize: function() {
+
+       this.$main = this.$('#main');
+
+       this.thoughts = new Thoughts();
+       this.thoughts.fetch({ reset: true });
+       this.thoughts.on('reset', this.render, this );
+    },
+
+    render: function() {
+      this.input = this.$('create-thought');
+      _.each( this.thoughts.models, function(thought){
+          this.$main.append( this.template( thought.toJSON() ) );
+      }, this);
+      return this;
+    },
+
+    createThought: function() {
+
+      var self = this;
+
+      var thought = new Thought();
+      thought.save({ description: this.input.val() }, {
+        success: function( model, rsp ) {
+
+          self.$main.prepend( self.template( model.toJSON() ) );
+
+        }
+      });
+    }
+
+  });
+
+  var thoughtView = new ThoughtView();
+
 });
