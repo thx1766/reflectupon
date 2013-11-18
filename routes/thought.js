aa57630aed2d01dae5c0ba65a721e8bb92f566db
Andrew Casal <andrewjcasal@gmail.com>
2013-11-17 17:10:47 -0800

    twilio cut

--- routes/thought.js
+++ routes/thought.js
@@ -92,6 +92,8 @@ exports.getTwiml = function(req, res) {
 
     exports.Thought.random(null,function(err, thought) {
 
+        thought.description = thought.description.substr(0,120) + "...";
+
         res.render('twiml', {reflection: thought.description} );
 
     })
