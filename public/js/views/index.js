de872d50701c085fbe454e9a67081dd0c765b535
Andrew Casal <andrewjcasal@gmail.com>
2016-02-07 13:43:28 -0800

    image gif

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -20,12 +20,112 @@ window.rupon.views = window.rupon.views || {};
             this.render();
 
             $(window).scroll(function() {
-                var scrollTop = $(document).scrollTop();
+                var scrollTop = $(document).scrollTop(),
+                    ind, pos;
 
                 if (scrollTop > 250) {
                     $('.navbar').addClass('scrolled');
                 } else {
                     $('.navbar').removeClass('scrolled');
+                }
+
+                ind = $("#level3 img").index($('#level3 img.selected'));
+                if (scrollTop > 700) {
+                    pos = 4;
+                    if (ind != pos) {
+                        $("#level3 img.selected").removeClass("selected");
+                        $("#level3 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 600) {
+                    pos = 3;
+                    if (ind != pos) {
+                        $("#level3 img.selected").removeClass("selected");
+                        $("#level3 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 500) {
+                    pos = 2;
+                    if (ind != pos) {
+                        $("#level3 img.selected").removeClass("selected");
+                        $("#level3 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 400) {
+                    pos = 1;
+                    if (ind != pos) {
+                        $("#level3 img.selected").removeClass("selected");
+                        $("#level3 img:eq("+pos+")").addClass("selected");
+                    }
+                } else {
+                    pos = 0;
+                    if (ind != pos) {
+                        $("#level3 img.selected").removeClass("selected");
+                        $("#level3 img:eq("+pos+")").addClass("selected");
+                    }
+                }
+
+                ind = $("#level4 img").index($('#level4 img.selected'));
+                if (scrollTop > 1300) {
+                    pos = 4;
+                    if (ind != pos) {
+                        $("#level4 img.selected").removeClass("selected");
+                        $("#level4 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 1200) {
+                    pos = 3;
+                    if (ind != pos) {
+                        $("#level4 img.selected").removeClass("selected");
+                        $("#level4 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 1100) {
+                    pos = 2;
+                    if (ind != pos) {
+                        $("#level4 img.selected").removeClass("selected");
+                        $("#level4 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 1000) {
+                    pos = 1;
+                    if (ind != pos) {
+                        $("#level4 img.selected").removeClass("selected");
+                        $("#level4 img:eq("+pos+")").addClass("selected");
+                    }
+                } else {
+                    pos = 0;
+                    if (ind != pos) {
+                        $("#level4 img.selected").removeClass("selected");
+                        $("#level4 img:eq("+pos+")").addClass("selected");
+                    }
+                }
+
+                ind = $("#level6 img").index($('#level6 img.selected'));
+                if (scrollTop > 2200) {
+                    pos = 4;
+                    if (ind != pos) {
+                        $("#level6 img.selected").removeClass("selected");
+                        $("#level6 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 2100) {
+                    pos = 3;
+                    if (ind != pos) {
+                        $("#level6 img.selected").removeClass("selected");
+                        $("#level6 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 2000) {
+                    pos = 2;
+                    if (ind != pos) {
+                        $("#level6 img.selected").removeClass("selected");
+                        $("#level6 img:eq("+pos+")").addClass("selected");
+                    }
+                } else if (scrollTop > 1900) {
+                    pos = 1;
+                    if (ind != pos) {
+                        $("#level6 img.selected").removeClass("selected");
+                        $("#level6 img:eq("+pos+")").addClass("selected");
+                    }
+                } else {
+                    pos = 0;
+                    if (ind != pos) {
+                        $("#level6 img.selected").removeClass("selected");
+                        $("#level6 img:eq("+pos+")").addClass("selected");
+                    }
                 }
             })
         },
