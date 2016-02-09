a6565ec652ac2fd459c75294a268afc8cdb842ba
Andrew Casal <andrewjcasal@gmail.com>
2016-02-09 13:53:40 -0800

    don't scroll up on mobile

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -29,7 +29,7 @@ window.rupon.views = window.rupon.views || {};
                 var docWidth = $(document).width();
                 var scrollDict = {};
 
-                if (scrollTop < 300) {
+                if (scrollTop < 300 && docWidth > 1200) {
                     $("#level1 img").css('margin-top', -100 * scrollTop/300);
                 }
 
