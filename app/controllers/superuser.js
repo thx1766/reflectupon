72a3680fc714d0303a26bbe37b4059e5c7a5c97a
Andrew Casal <andrewjcasal@gmail.com>
2016-02-12 14:41:40 -0800

    fix for stephen

--- app/controllers/superuser.js
+++ app/controllers/superuser.js
@@ -1,5 +1,5 @@
 exports.get = function(req, res) {
-    var is_admin = req.user.username == 'andrew';
+    var is_admin = req.user.email == 'andrewjcasal@gmail.com' || req.user.email == "stranovich@gmail.com";
 
     if (is_admin) {
       res.render('superuser', { user: req.user, topBar: true, signout: true, landing_page: false, is_admin: is_admin });
