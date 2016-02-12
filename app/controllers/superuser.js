cfb1aaa8b803de9decb0d6d83542bf08c120a338
Andrew Casal <andrewjcasal@gmail.com>
2016-02-12 01:03:55 -0800

    fix undefined var

--- app/controllers/superuser.js
+++ app/controllers/superuser.js
@@ -1,6 +1,6 @@
 exports.get = function(req, res) {
-    var is_admin = req.user.username == 'andrew',
-        page;
+    var is_admin = req.user.username == 'andrew';
+
     if (is_admin) {
       res.render('superuser', { user: req.user, topBar: true, signout: true, landing_page: false, is_admin: is_admin });
     } else {
